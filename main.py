1b7a6ec285cf0a450d63b70fdb004db08f77ec08
tatchum237 <tatchumdefo@gmail.com>
2023-09-19 06:53:23 +0100

    second ticket fait

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # TATCHUM DEFO MICHEL ULRICH MASTER2 IA DIT
 import os
-
+import nbformat
 
 class Dossier_Fichier:
 
@@ -11,6 +11,24 @@ class Dossier_Fichier:
     def create_dossier(self, name,  name_dossier=""):
         src_dir = os.path.join(self.project_root+""+ name_dossier, name)
         os.makedirs(src_dir, exist_ok=True)
+
+    def create_fichier(self, name, extension, name_dossier, code_content="""
+        # Votre code d'analyse de données ici"""):
+
+        if(extension == ".py" or extension == ".md" or 
+           extension == ".txt"):
+            path = os.path.join(self.project_root+''+name_dossier, name+""+extension)
+            with open(path, 'w') as code_file:
+                code_file.write(code_content)
+
+        else:
+            notebook = nbformat.v4.new_notebook()
+            code_cell = nbformat.v4.new_code_cell(source='print("Hello, World!")')
+            notebook.cells.append(code_cell)
+            code_file_path = os.path.join(self.project_root+""+name_dossier, name+''+extension)
+            with open(code_file_path, 'w') as code_file:
+                nbformat.write(notebook, code_file)
+  
     
 
 
